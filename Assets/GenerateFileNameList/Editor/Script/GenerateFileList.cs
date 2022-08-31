fed8325aa12c18bd2fc613de2d7d8bb6dd0877a6
Yamada <ttyyamada0813@gmail.com>
2022-08-31 14:57:31 +0900

    Dictionary出力対応

--- Assets/GenerateFileNameList/Editor/Script/GenerateFileList.cs
+++ Assets/GenerateFileNameList/Editor/Script/GenerateFileList.cs
@@ -26,12 +26,16 @@ namespace GenerateFileList
         private string _outputFileName = "FileNameList";
         // 名前空間名
         private string _outputNameSpaceName = "GenerateFileList";
+        private CreateMode _currentCreateMode = CreateMode.変数;
+        
         // クラス名
         private string _outputClassName = "FileNames";
-        private string _codeTemplate;
+        private string _codeTemplateFileList;
+        private string _codeTemplateFileDictionary;
         
         // 置き換える対象のリスト
         private static readonly string CodeTemplateFileName = "NameListsTemplate";
+        private static readonly string CodeTemplateFileNameDictionary = "NameDictionaryTemplate";
         private static readonly string NameSpaceReplaceTarget = "#NAMESPACE#";
         private static readonly string ClassNameReplaceTarget = "#CLASSNAME#";
         private static readonly string FileNameReplaceTarget = "#FILENAME#";
@@ -40,7 +44,15 @@ namespace GenerateFileList
         private static readonly string FileListTemplate =
             "        public static readonly string #FILENAME# = \"#filename#\";";
 
+        private static readonly string FileDictionaryTemplate = 
+            "                {\"#FILENAME#\", \"#filename#\"},";
+
         private string _nameLineTemplate;
+
+        private static readonly string OutputFileNameKey = "generate_file_list_output_file_name_key";
+        private static readonly string OutputClassNameKey = "generate_file_list_output_class_name_key";
+        private static readonly string OutputNameSpaceKey = "generate_file_list_output_namespace_key";
+        private static readonly string CreateTypeKey = "generate_file_list_create_file_type_key";
 
         [MenuItem("Editor/GenerateFileNameList")]
         private static void CreateWindow()
@@ -51,13 +63,18 @@ namespace GenerateFileList
         private void OnEnable()
         {
             _currentWindow = this;
-            _codeTemplate = Resources.Load<TextAsset>(CodeTemplateFileName).text;
+            _codeTemplateFileList = Resources.Load<TextAsset>(CodeTemplateFileName).text;
+            _codeTemplateFileDictionary = Resources.Load<TextAsset>(CodeTemplateFileNameDictionary).text;
+            _outputFileName = PlayerPrefs.GetString(OutputFileNameKey, _outputFileName);
+            _outputClassName = PlayerPrefs.GetString(OutputClassNameKey, _outputClassName);
+            _outputNameSpaceName = PlayerPrefs.GetString(OutputNameSpaceKey, _outputNameSpaceName);
+            _currentCreateMode = (CreateMode)PlayerPrefs.GetInt(CreateTypeKey, (int)_currentCreateMode);
         }
 
         private void OnGUI()
         {
             GUILayout.BeginArea(new Rect(0, 0, _currentWindow.position.size.x, _currentWindow.position.size.y));
-            if (_codeTemplate == null)
+            if (_codeTemplateFileList == null || _codeTemplateFileDictionary == null)
             {
                 EditorGUILayout.HelpBox("コードテンプレートの読み込みエラー", MessageType.Error);
                 GUILayout.EndArea();
@@ -70,6 +87,7 @@ namespace GenerateFileList
             }
             else
             {
+                _currentCreateMode = (CreateMode) EditorGUILayout.EnumPopup("生成タイプ", _currentCreateMode);
                 _outputFileName = EditorGUILayout.TextField("ファイル名(拡張子は無し)", _outputFileName);
                 _outputNameSpaceName = EditorGUILayout.TextField("namaspace名", _outputNameSpaceName);
                 _outputClassName = EditorGUILayout.TextField("クラス名", _outputClassName);
@@ -151,17 +169,38 @@ namespace GenerateFileList
                 }
                 // 変数名に使うものは空白を削除
                 nameWithoutExtension = nameWithoutExtension.Replace(" ", "");
-                var newText = FileListTemplate.Replace(FileNameReplaceTarget, nameWithoutExtension)
+
+                var replaceTarget = GetTemplateByCreateMode();
+                var newText = replaceTarget.Replace(FileNameReplaceTarget, nameWithoutExtension)
                     .Replace(FileNameValueReplaceTarget, targetName);
                 listString += newText;
             }
 
             // リスト部分を置き換える
-            generateClassText = generateClassText.Replace(FileListTemplate, listString);
+            generateClassText = generateClassText.Replace(GetTemplateByCreateMode(), listString);
 
             // ファイルを生成する
             File.WriteAllText($"{AssetDatabase.GetAssetOrScenePath(_createTargetFolder)}/{_outputFileName}.cs" , generateClassText);
+            SaveKey();
             AssetDatabase.Refresh();
+        }
+
+        /// <summary>
+        /// 今の生成モードにあったテンプレートを取得する
+        /// </summary>
+        /// <returns></returns>
+        private string GetTemplateByCreateMode()
+        {
+            switch (_currentCreateMode)
+            {
+                case CreateMode.変数:
+                    return FileListTemplate;
+                case CreateMode.Dictionary:
+                    return FileDictionaryTemplate;
+            }
+            
+            Debug.LogError("生成モード判定エラー！");
+            return null;
         }
 
         /// <summary>
@@ -169,11 +208,38 @@ namespace GenerateFileList
         /// </summary>
         private string ReplaceCodeTemplate()
         {
-            var generateClassText = _codeTemplate;
+            var generateClassText = _codeTemplateFileList;
+            switch (_currentCreateMode)
+            {
+                case CreateMode.変数:
+                    generateClassText = _codeTemplateFileList;
+                    break;
+                case CreateMode.Dictionary:
+                    generateClassText = _codeTemplateFileDictionary;
+                    break;
+            }
             generateClassText = generateClassText.Replace(NameSpaceReplaceTarget, _outputNameSpaceName);
             generateClassText = generateClassText.Replace(ClassNameReplaceTarget, _outputClassName);
 
             return generateClassText;
+        }
+
+        private void SaveKey()
+        {
+            PlayerPrefs.SetString(OutputFileNameKey, _outputFileName);
+            PlayerPrefs.SetString(OutputClassNameKey, _outputClassName);
+            PlayerPrefs.SetString(OutputNameSpaceKey, _outputNameSpaceName);
+            PlayerPrefs.SetInt(CreateTypeKey, (int)_currentCreateMode);
+            PlayerPrefs.Save();
+        }
+
+        /// <summary>
+        /// ファイルの生成モード
+        /// </summary>
+        private enum CreateMode
+        {
+            変数,
+            Dictionary,
         }
     }
 }
